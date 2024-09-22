1c405012866becf64f93afd1af5a00065bf19aaf
parthiban <parthimyself90@gmail.com>
2024-09-22 22:15:58 +0530

    create p2p transport

---
+++ p2p/tcp_transport.go
@@ -0,0 +1,60 @@
+package p2p
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type TCPPeer struct {
+	conn     net.Conn
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn:     conn,
+		outbound: outbound,
+	}
+}
+
+type TCPTransport struct {
+	listenAddress string
+	listener      net.Listener
+
+	mu    sync.RWMutex
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+	go t.startAcceptLoop()
+	return nil
+
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+		}
+		go t.handleconn(conn)
+	}
+}
+
+func (t *TCPTransport) handleconn(conn net.Conn) {
+	peer := NewTCPPeer(conn, true)
+	fmt.Printf("New Incomming Connection %+v\n", peer)
+}
